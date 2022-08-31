36550f9f6792ea0eeddc28bc1cb0f7e5dedb002b
Andrii Kuz <andrii@MacBook-Air-Andrii.local>
2022-08-31 14:44:48 +0300

    Heroku

---
+++ src/services/MarvelService.js
@@ -0,0 +1,50 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=bf5f7aca912ddbd2a1ec016fb5c14284";
+  _baseOffset = 210;
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+    return await res.json();
+  };
+
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    if (char.description.length > 210) {
+      char.description = char.description.slice(0, 210) + " ...";
+    }
+
+    if (!char.description) {
+      char.description = "Data about this character is not found.";
+    }
+
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items,
+    };
+  };
+}
+
+export default MarvelService;
