a044bab1dcc58c31f7f212384fb5dc6a805555c0
Andrii Kuz <andrii@MacBook-Air-Andrii.local>
2022-09-16 19:23:11 +0300

    Work with animation. Installed react-transition-group package

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useMarvelService from "../../services/MarvelService";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
@@ -37,21 +38,27 @@ const ComicsList = () => {
   function renderItems(arr) {
     const items = arr.map((item, i) => {
       return (
-        <li className="comics__item" key={i}>
-          <Link to={`/comics/${item.id}`}>
-            <img
-              src={item.thumbnail}
-              alt={item.title}
-              className="comics__item-img"
-            />
-            <div className="comics__item-name">{item.title}</div>
-            <div className="comics__item-price">{item.price}</div>
-          </Link>
-        </li>
+        <CSSTransition key={i} timeout={500} classNames="comics__item">
+          <li className="comics__item" key={i}>
+            <Link to={`/comics/${item.id}`}>
+              <img
+                src={item.thumbnail}
+                alt={item.title}
+                className="comics__item-img"
+              />
+              <div className="comics__item-name">{item.title}</div>
+              <div className="comics__item-price">{item.price}</div>
+            </Link>
+          </li>
+        </CSSTransition>
       );
     });
 
-    return <ul className="comics__grid">{items}</ul>;
+    return (
+      <ul className="comics__grid">
+        <TransitionGroup component={null}>{items}</TransitionGroup>
+      </ul>
+    );
   }
 
   const items = renderItems(comicsList);
